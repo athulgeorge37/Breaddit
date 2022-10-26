36530cf41be6bbb5c290c0454caa638da1331164
AthulGeorge117 <s3918048@student.rmit.edu.au>
2022-10-26 19:29:57 +1100

    cleans up server.js code, removes unnessary packages

--- server/schema/Resolvers.js
+++ server/schema/Resolvers.js
@@ -1,4 +1,3 @@
-const { request } = require("express");
 const db = require("../models");
 
 const { Op } = require("sequelize");
--- server/server.js
+++ server/server.js
@@ -1,60 +1,51 @@
-const { typeDefs } = require("./schema/TypeDefs");
-const { resolvers } = require("./schema/Resolvers");
-
-const { ApolloServer } = require("apollo-server-express");
-const { ApolloServerPluginDrainHttpServer } = require("apollo-server-core");
-const { WebSocketServer } = require("ws");
-const { useServer } = require("graphql-ws/lib/use/ws");
-
-const { makeExecutableSchema } = require("@graphql-tools/schema");
-// const gql = require("graphql-tag");
+// creating our express app
 const express = require("express");
-const http = require("http");
-const { PubSub } = require("graphql-subscriptions");
-// may need to install ws for graphql-ws to work
-
-// Required logic for integrating with Express
 const app = express();
+
+// allows anything returned by express to be in JSON format
 app.use(express.json());
 
 // allows us to send api requests from the client
 const cors = require("cors");
 app.use(cors());
 
-// go into ./config/config.js to see the db connection information
-// this require below will run the ./models/index.js file, to start db
-const db = require("./models");
-
 // Routers:
 // each route gives the app access to the sequlize queries for that route
-const userRoutes = require("./routes/User-route");
-app.use("/api/user", userRoutes);
 // client can send an axios request to
 // http://localhost:3001/api/user + (route endpoint) within userRoute
 // to get, post, delete, put data in db
 
+const userRoutes = require("./routes/User-route");
 const postRoutes = require("./routes/Post-route");
-app.use("/api/post", postRoutes);
-
 const commentRoutes = require("./routes/Comment-route");
-app.use("/api/comment", commentRoutes);
-
 const voteRoutes = require("./routes/Vote-route");
-app.use("/api/vote", voteRoutes);
-
 const followerRoutes = require("./routes/Follower-route");
-app.use("/api/follower", followerRoutes);
-
 const emailRoutes = require("./routes/Email-route");
-app.use("/api/email", emailRoutes);
-
 const profileVisitRoutes = require("./routes/ProfileVisits-route");
+
+app.use("/api/user", userRoutes);
+app.use("/api/post", postRoutes);
+app.use("/api/comment", commentRoutes);
+app.use("/api/vote", voteRoutes);
+app.use("/api/follower", followerRoutes);
+app.use("/api/email", emailRoutes);
 app.use("/api/profile_visits", profileVisitRoutes);
 
-const httpServer = http.createServer(app);
+// graphql typedefs and resolvers, required for our apollo server
+const { makeExecutableSchema } = require("@graphql-tools/schema");
+const { typeDefs } = require("./schema/TypeDefs");
+const { resolvers } = require("./schema/Resolvers");
 const schema = makeExecutableSchema({ typeDefs, resolvers });
+
+// adding http to our app server
+const http = require("http");
+const httpServer = http.createServer(app);
+
+const { PubSub } = require("graphql-subscriptions");
 const pubsub = new PubSub();
 
+// websocket server where the subcriptions will run on
+const { WebSocketServer } = require("ws");
 const wsServer = new WebSocketServer({
     // This is the `httpServer` returned by createServer(app);
     server: httpServer,
@@ -63,12 +54,15 @@ const wsServer = new WebSocketServer({
     path: "/graphql",
 });
 
+const { useServer } = require("graphql-ws/lib/use/ws");
 // Passing in an instance of a GraphQLSchema and
 // telling the WebSocketServer to start listening
 const serverCleanup = useServer(
     {
         schema,
         context: () => {
+            // giving access to the pubsub throgh the context param
+            // for our subscription listener
             return {
                 pubsub,
             };
@@ -77,9 +71,12 @@ const serverCleanup = useServer(
     wsServer
 );
 
+const { ApolloServer } = require("apollo-server-express");
+const { ApolloServerPluginDrainHttpServer } = require("apollo-server-core");
 // Same ApolloServer initialization as before, plus the drain plugin.
 const server = new ApolloServer({
     schema,
+    // passing the same pubsub so our apollo server can publish a subscription event
     context: ({ req, res }) => ({ req, res, pubsub }),
     plugins: [
         ApolloServerPluginDrainHttpServer({ httpServer }),
@@ -95,6 +92,10 @@ const server = new ApolloServer({
     ],
 });
 
+// go into ./config/config.js to see the db connection information
+// this require below will run the ./models/index.js file, to start db
+const db = require("./models");
+// sync will make sure our db tables are synced with the database
 db.sequelize.sync().then(() => {
     // db.sequelize.sync({ force: true });
 
@@ -104,9 +105,10 @@ db.sequelize.sync().then(() => {
     // server when we make changes and save
 
     server.start().then((response) => {
-        // so we can use our server for graphql with app
+        // so we can use our apollo server for graphql with our app server
         server.applyMiddleware({ app });
 
+        // httpServer contains both subscription server, apollo server and app server
         httpServer.listen(3001, () => {
             console.log("server running on port 3001");
         });
